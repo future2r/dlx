d59cc7a776bff654e2628286554dbd3ca9df584e
Frank Ulbricht <frank@ulbricht.name>
2026-04-10 11:36:10 +0200

    Use unified stage

--- src/main/java/name/ulbricht/dlx/ui/view/main/MainView.java
+++ src/main/java/name/ulbricht/dlx/ui/view/main/MainView.java
@@ -2,10 +2,14 @@ package name.ulbricht.dlx.ui.view.main;
 
 import static java.util.Objects.requireNonNull;
 
+import javafx.application.ConditionalFeature;
+import javafx.application.Platform;
 import javafx.beans.property.ReadOnlyStringProperty;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import javafx.stage.WindowEvent;
 import name.ulbricht.dlx.ui.stage.Stages;
 import name.ulbricht.dlx.ui.view.View;
@@ -37,12 +41,19 @@ public final class MainView implements View<Parent, MainViewModel> {
         final var scene = new Scene(view.getRoot());
 
         // Configure the stage
+        if (Platform.isSupported(ConditionalFeature.UNIFIED_WINDOW)) {
+            stage.initStyle(StageStyle.UNIFIED);
+            scene.setFill(Color.TRANSPARENT);
+        }
         stage.setScene(scene);
         Stages.initStageIcons(stage);
+        stage.setMinWidth(500);
+        stage.setMinHeight(400);
         stage.titleProperty().bind(view.titleProperty());
 
         ViewServices.userPreferences().getStageState(STAGE_ID)
-                .ifPresentOrElse(state -> Stages.restoreStageState(stage, state), () -> stage.centerOnScreen());
+                .ifPresentOrElse(state -> Platform.runLater(() -> Stages.restoreStageState(stage, state)),
+                        () -> stage.centerOnScreen());
 
         // Forward window events to the controller
         stage.addEventHandler(WindowEvent.WINDOW_SHOWN, controller::windowShown);
