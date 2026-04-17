9184e829433210ebb14c027596f1f547c7058bfb
Frank Ulbricht <frank@ulbricht.name>
2026-04-17 10:58:58 +0200

    Format example code

--- src/main/resources/name/ulbricht/dlx/ui/view/editor/example.s
+++ src/main/resources/name/ulbricht/dlx/ui/view/editor/example.s
@@ -1,14 +1,14 @@
-; Example DLX Program
+; Example program that adds two values
 
-        .data       ; data section
-a:      .word 10    ; operand 1
-b:      .word 32    ; operand 2
-res:    .word 0     ; result
+        .data           ; data section
+a:      .word 10        ; operand 1
+b:      .word 32        ; operand 2
+res:    .word 0         ; result
 
         .text           ; program section
-main:
+main:                   ; main entry point
         lw r2, a(r0)    ; load 'a' into R2
         lw r3, b(r0)    ; load 'b' into R3
-        add r4, r2, r3  ; R2 + R3 = R4
+        add r4, r2, r3  ; R4 = R2 + R3
         sw res(r0), r4  ; store R4 into 'res'
         trap 0          ; end program
